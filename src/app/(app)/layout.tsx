71fa03fefcd5e4bc30728dde22d60046c338fe3c
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-11-18 00:07:37 +0530

    Dark Mode

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "@/styles/globals.css";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -4,11 +4,16 @@ import Link from "next/link";
 import { PlusIcon } from "lucide-react";
 import { useState } from "react";
 import { UserButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 import { Button } from "@/components/ui/button";
 import AddEditNoteDialog from "@/components/notes/AddEditNoteDialog";
+import { ThemeButton } from "@/components/ui/theme-button";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+  const { theme } = useTheme();
+
   const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
 
   return (
@@ -28,9 +33,12 @@ export default function Navbar() {
               Add Note
             </Button>
 
+            <ThemeButton />
+
             <UserButton
               afterSignOutUrl="/"
               appearance={{
+                baseTheme: theme === "dark" ? dark : undefined,
                 elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
               }}
             />
